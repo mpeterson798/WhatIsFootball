ef6f52abc8d58336c3c9b385e5c722c3c2fe968f
Matthew Peterson <matthew251125@icloud.com>
2025-09-04 20:47:09 -0500

    Initial Commit

---
+++ WhatIsFootball/WhatIsFootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatIsFootballApp.swift
+//  WhatIsFootball
+//
+//  Created by Matthew Peterson on 9/4/25.
+//
+
+import SwiftUI
+
+@main
+struct WhatIsFootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
